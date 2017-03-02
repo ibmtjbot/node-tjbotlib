ca77992db1c09256a698acc6b1aaf13653d70d69
Victor <dibiavc@us.ibm.com>
2017-03-02 18:15:03 -0500

    remove quotes

--- samples/config.default.js
+++ samples/config.default.js
@@ -1,6 +1,9 @@
 // User-specific CONFIGURATION
 exports.config = {}
-exports.config.voice = {"gender": "female", "language": "en-US"};
+exports.config.voice = {
+    "gender": "female",
+    "language": "en-US"
+};
 exports.config.conversationWorkspaceId = ''; // replace with the workspace identifier of your conversation
 
 // Card and Device found in command arecord -l of your hardware. In general 0,0 or 1,0 or 0,1
@@ -23,28 +26,27 @@ exports.config.cameraParams = {
 }
 
 // Create the CREDENTIALS object for export
-exports.credentials = {
-};
+exports.credentials = {};
 
 // Watson Speech to Text
 // https://www.ibm.com/watson/developercloud/speech-to-text.html
 exports.credentials.speech_to_text = {
-  "username": "",
-  "password": ""
+    username: "",
+    password: ""
 };
 
 // Watson Text to Speech
 // https://www.ibm.com/watson/developercloud/text-to-speech.html
 exports.credentials.text_to_speech = {
-  "username": "",
-  "password": ""
+    username: "",
+    password: ""
 };
 
 // Watson Conversation
 // https://www.ibm.com/watson/developercloud/conversation.html
 exports.credentials.conversation = {
-  "username": "",
-  "password": ""
+    username: "",
+    password: ""
 };
 
 // Watson Vision Recognition
