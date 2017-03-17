906dfc07c487338993ab5d48e36771e38cc8ba0d
Victor <dibiavc@us.ibm.com>
2017-03-16 21:32:57 -0400

    improve config.js structure

---
+++ samples/capturetest.js
@@ -0,0 +1,25 @@
+// raspistill - o shot - $SHOTTIME.jpg--nopreview--exposure sports--timeout 1
+
+var starttime = Date.now();
+var endtime;
+var cameraParams = [];
+var filePath = "bingo.jpg"
+cameraParams.push('-vf');
+cameraParams.push('-hf');
+cameraParams.push('-o', filePath);
+cameraParams.push('-t', 1)
+
+starttime = Date.now();
+var spawn = require('child_process').spawn('raspistill', cameraParams);
+spawn.on('exit', function(code) {
+    endtime = Date.now();
+    console.info("> saved [" + filePath + "]" + " exit code, " + code + " time: ", (endtime - starttime) / 1000);
+
+});
+
+spawn.on('error', function() {
+    if (self.config.verboseLogging) {
+        console.error("> there was an error capturing image [" + filePath + "]");
+    }
+    reject();
+});
