1eac8e6fcee201a88aaadbc280e934b91235a88c
Justin Weisz <jweisz@us.ibm.com>
2017-03-23 11:47:49 -0400

    deleting older code

--- test/test.speaker.js
+++ test/test.speaker.js
@@ -1,21 +1,3 @@
-'use strict';
-var expect = require('chai').expect;
-var tjbot = require('../lib/tjbot')
-var tj = new tjbot(['speaker', 'microphone'], {}, {});
-
-
-describe('#tjbot SPEAKER', function() {
-    it('should play a wave sound', function() {
-        var soundFile = "/usr/share/sounds/alsa/Front_Center.wav";
-        var result = tj.playSound(soundFile).then(function(result) {
-            expect(result).equal(soundFile);
-        });
-    });
-});
-
-// var endTime = new Date();
-// console.log("time to shine led ", startTime, endTime, (startTime - endTime) / 1000)
-
 /**
  * Copyright 2016 IBM Corp. All Rights Reserved.
  *
