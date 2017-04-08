5b144246d47da19becc03a252713aa09bf270d66
Victor <dibiavc@us.ibm.com>
2017-04-07 21:48:07 -0400

    update lib add takePhotoCapability

---
+++ samples/test.js
@@ -0,0 +1,25 @@
+var tjbot = require('../lib/tjbot');
+var config = require('./config');
+
+// obtain our credentials from config.js
+var credentials = config.credentials;
+
+// obtain user-specific config
+//var VOICE = constants.config.voice;
+var WORKSPACEID = config.conversationWorkspaceId;
+
+// these are the hardware capabilities that TJ needs for this recipe
+var hardware = ['microphone', 'speaker', 'led', 'servo', "camera"];
+
+// set up configuration paramters
+var config = {
+    log: {
+        level: 'verbose'
+    }
+};
+// instantiate our TJBot!
+var tj = new tjbot(hardware, config, credentials);
+tj.takePhoto("test").then(function(path) {
+    console.log("saved file to path", path)
+})
+tj.see();
