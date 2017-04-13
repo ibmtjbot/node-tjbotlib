29ec47f2d614b1821333a1808e0465600212bc86
Justin Weisz <jweisz@us.ibm.com>
2017-04-13 11:44:44 -0400

    fixed a bug where calling listen() would throw an error after calling stopListen()

---
+++ test/config.default.js
@@ -0,0 +1,9 @@
+// Create the credentials object for export
+exports.credentials = {};
+
+// Watson Speech to Text
+// https://www.ibm.com/watson/developercloud/speech-to-text.html
+exports.credentials.speech_to_text = {
+    password: '',
+    username: ''
+};
