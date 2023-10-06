b45cc30905e122962897e343d23ba12ab792e76c
Justin Weisz <jweisz@us.ibm.com>
2023-10-06 16:51:12 -0400

    Removed non-core Watson services, updated dependency versions

--- __tests__/test.tjbotlib.js
+++ __tests__/test.tjbotlib.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /**
- * Copyright 2016-2020 IBM Corp. All Rights Reserved.
+ * Copyright 2016-2023 IBM Corp. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
  */
 
 import TJBot from '../src/tjbot.js';
-import config from './config.js';
 
 test('instantiate TJBot', () => {
     const tjbot = new TJBot();
@@ -36,32 +35,6 @@ test('instantiate TJBot with credentials file', () => {
     // eslint-disable-next-line no-unused-vars
     const tjbot = new TJBot({}, 'ibm-credentials.env');
     expect(process.env.IBM_CREDENTIALS_FILE).toEqual('ibm-credentials.env');
-});
-
-test('instantiate TJBot with Assistant', async () => {
-    const tjbot = new TJBot({
-        log: { level: 'info' },
-        converse: { assistantId: config.assistantId }
-    }, 'ibm-credentials.env');
-    tjbot.initialize([]);
-    let data = await tjbot.converse("hello");
-    expect(data.description).toBe("Hello");
-});
-
-test('instantiate TJBot with Tone Analyzer/NLU', async () => {
-    const tjbot = new TJBot({}, 'ibm-credentials.env');
-    tjbot.initialize([]);
-    let tone = await tjbot.analyzeTone("I am so happy to see you! Thank you for being here!");
-    const tones = [];
-    Object.keys(tone.emotion.document.emotion).forEach(key => {
-        let value = tone.emotion.document.emotion[key];
-        tones.push({tone_id: key, score: value});
-    })
-    const emotionalTones = tones.filter((t) => t.tone_id === 'anger' || t.tone_id === 'fear' || t.tone_id === 'joy' || t.tone_id === 'sadness');
-        if (emotionalTones.length > 0) {
-            const maxTone = emotionalTones.reduce((a, b) => ((a.score > b.score) ? a : b));
-            expect(maxTone.tone_id).toBe('joy');
-        }
 });
 
 test('instantiate TJBot with no hardware', () => {
